f5db3e51429b059450f5097066faf1fda7bbc25e
Lucas-Miranda-BR <lucasmirandabr0508@gmail.com>
2025-02-23 21:22:21 -0300

    Modificações - Subpastas - Exercicios realizados novos

---
+++ 2_Formularios/index.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Formulários</title>
+</head>
+<body>
+
+    <form action="index.php" method="get">
+
+        <fieldset>
+            <legend>Informações do Personagem</legend>
+
+            <label for="nome">Nome:</label>
+            <input type="text" name="nome" id="nome" placeholder="Nome do personagem" required autocomplete="off">
+
+            <br>
+
+            <label for="poder">Poder:</label>
+            <input type="text" name="poder" id="poder" placeholder="Poder do personagem" required autocomplete="off">
+
+            <br>
+
+            <input type="submit" value="Enviar">
+        </fieldset>
+
+    </form>
+
+    <?php
+
+    if (isset($_GET[`nome`])) {
+        if (empty($_GET[`nome`])) {
+            echo `O nome é obrigatórtio!`;
+        }
+        else {
+            echo $_GET[`nome`];
+        }
+    }
+    
+    if (isset($_GET[`poder`])) {
+        if (empty($_GET[`poder`])) {
+            echo `O poder é obrigatórtio!`;
+        }
+        else {
+            echo $_GET[`poder`];
+        }
+    } 
+        
+    ?>
+
+</body>
+</html>
---
+++ 3_Calculadora/index.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Calculadora simples</title>
+</head>
+<body>
+
+    <form action="index.php" method="get">
+
+        <fieldset>
+            <legend>Calculadora simples</legend>
+
+            <label for="valor_1">Primeiro valor:</label>
+            <input type="number" name="valor_1" id="valor_1" placeholder="Número" required autocomplete="off">
+
+            <br>
+
+            <label for="valor_2">Segundo valor:</label>
+            <input type="number" name="valor_2" id="valor_2" placeholder="Número" required autocomplete="off">
+
+            <br>
+
+            <input type="submit" name="somar" value="Somar">
+            <input type="submit" name="subtrair" value="Subtrair">
+            <input type="submit" name="multiplicar" value="Multiplicar">
+            <input type="submit" name="dividir" value="Dividir">
+        </fieldset>
+
+    </form>
+
+    <?php
+
+
+    if (isset($_GET['valor_1'])) {
+        if (empty($_GET['valor_1'])) {
+            echo 'O primeiro valor é obrigatórtio!';
+            exit();
+        }
+    }
+    else {
+        exit();
+    }
+    
+    if (isset($_GET['valor_2'])) {
+        if (empty($_GET['valor_2'])) {
+            echo 'O segundo valor é obrigatórtio!';
+            exit();
+        }
+    } 
+    else {
+        exit();
+    }
+        if (isset($_GET ['somar']))
+            echo $_GET['valor_1'] + $_GET['valor_2'];
+
+        if (isset($_GET ['subtrair']))
+            echo $_GET['valor_1'] - $_GET['valor_2'];
+
+        if (isset($_GET ['multiplicar']))
+            echo $_GET['valor_1'] * $_GET['valor_2'];
+
+        if (isset($_GET ['dividir']))
+            echo $_GET['valor_1'] / $_GET['valor_2'];
+
+    ?>
+
+</body>
+</html>
