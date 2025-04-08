3947ec0001ea5d56fb01c9e9fb1c99c3cff5dafb
Lucas-Miranda-BR <lucasmirandabr0508@gmail.com>
2025-04-08 16:27:29 -0300

    Refazendo Exercicios

--- PHP/8_Excs_PHP_2/exc03.php
+++ PHP/8_Excs_PHP_2/exc03.php
@@ -2,54 +2,68 @@
 
 $etec = [
 
-    "curso" => [
-        "nome" => "DS",
-        "DS" => [
-            "materia_pam" => "PAM",
-            "PAM" => [
-                "pam_mod1" => "Modulo 1 PAM",
-                "pam_mod2" => "Modulo 2 PAM"
-            ];
-            "materia_pw" => "PW",
-            "PW" => [
-                "pw_mod" = "Modulo PW"
-            ];
-            "materia_tpa" => "TPA",
-            "materia_ds_ing" => "Inglês"
-        ];
-        
-        "nome" => "ADM",
-        "ADM" => [
-            "materia_rh" => "RH",
-            "materia_gp" => "GP",
-            "materia_ta" => "TA",
-            "materia_adm_ing" => "Inglês"
-        ];
+    [
+        "curso" => "Desenvolvimento de Sistemas",
+        $DS = [
+        "materiapam" => "Programação de Aplicativos Mobile",
+        "materiapw" => "Programação Web",
+        "materiatpa" => "Técnicas Avançadas de Programação de Algoritmos",
+        "materiaing" => "Inglês",
 
-    ];
+        "modulopam" => "PAM",
+        "modulopw" => "PW",
+        "moduloing" => "ING"
+        ],
+
+        $PAM = [
+            "pammodulo1" => "Modulo I",
+            "pammodulo2" => "Modulo II"
+        ],
+
+        $PW = [
+           "pwmodulo1" => "Modulo I",
+           "pwmodulo2" => "Modulo II",
+           "pwmodulo3" => "Modulo III",
+           "pwmodulo4" => "Modulo IV"
+        ],
+
+        $ING => [
+           "ingmodulo1" => "Modulo I",
+           "ingmodulo2" => "Modulo II",
+           "ingmodulo3" => "Modulo III"
+        ],
+        
+    ],
+
+    [
+        "curso" => "Adminstração",
+        $ADM = [
+            "materiarh" => "Recursos Humanos",
+            "materiagp" => "Gestão de Pessoas",
+            "materiatpa" => "Teoria da Adminstração",
+            "materiaing" => "Inglês",
+            "modulorh" => "RH",
+            "moduloing" => "ING"
+        ],
+        $RH => [
+            "rhmodulo1" => "Modulo I",
+            "rhmodulo1" => "Modulo II"
+        ],
+        $ING => [
+                "ingmodulo1" => "Modulo I"
+        ],
+    ],
+];
 
     foreach ($etec as $key => $value) {
-        echo $value['curso'];
-        foreach($value['curso'] as $key => $value) {
-            echo "<hr> <br>";
-            echo $value['materia_pam'] " <br> ";
-            echo $value['pam_mod1'] " <br> ";
-            echo $value['pam_mod2'] " <br> ";
-            echo "<br>";
-            echo $value['materia_pw'] " <br> ";
-            echo $value['pw_mod'] " <br> ";
-            echo "<br>";
-            echo $value['materia_tpa'] " <br> ";
-            echo "<br>";
-            echo $value['materia_ds_ing'] " <br> ";
-            echo "<hr> <br>";
-            echo $value['materia_rh'] " <br> ";
-            echo $value['materia_gp'] " <br> ";
-            echo $value['materia_ta'] " <br> ";
-            echo $value['materia_adm_ing'] " <br> ";
-         }
-    }
-
-];
+        echo "Curso: " . $value['curso'] . "<br>";
+        foreach ($DS as $key => $value) {
+            echo "Materia PAM: " . $value['materiapam'];
+            echo "Materia PW: " . $value['materiapw'];
+            echo "Materia TPA: " . $value['materiatpa'];
+            echo "Materia ING: " . $value['materiaing'];
+        }
+            
+        }
 
 ?>
